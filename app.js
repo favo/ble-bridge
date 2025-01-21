406e5892537cc46584e0959abb7d27749d85faed
Jon Håvard Gundersen <jonh@favo.no>
2025-01-21 09:53:54 +0100

    Ryddet litt

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const bleno = require('@stoprocent/bleno');
+const bleno = require("@stoprocent/bleno");
 const { Server } = require("socket.io");
 const http = require("http");
 
@@ -6,50 +6,60 @@ const SERVICE_UUID = "89496822200000000000000000000000";
 const WRITE_CHARACTERISTIC_UUID = "89496822201000000000000000000000";
 const NOTIFY_CHARACTERISTIC_UUID = "89496822202000000000000000000000";
 
-const bleCallbacks = {
-    writeCallback: (data) => {},
-    notifyCallback: (isSubscribed, maxValueSize, callback) => {},
-};
+const EVENT_WRITE = "write"
+const EVENT_BLE_ENABLED = "ble-enabled"
+const EVENT_BLE_DISABLED = "ble-disabled"
+const EVENT_DEVICE_ACCEPTED = "device-accepted"
+const EVENT_DEVICE_DISCONNECTED = "device-disconnected"
 
-const writeCharacteristic = new bleno.Characteristic({
-    uuid: WRITE_CHARACTERISTIC_UUID,
-    properties: ["write"],
-    onWriteRequest: (data, _offset, _withoutResponse, callback) => {
-        console.log("onWriteRequest write request: " + data.toString("utf-8"));
-        bleCallbacks.writeCallback(data);
-        callback(bleno.Characteristic.RESULT_SUCCESS);
-    },
-});
-
-const notifyCharacteristic = new bleno.Characteristic({
-    uuid: NOTIFY_CHARACTERISTIC_UUID,
-    properties: ["notify"],
-    onSubscribe: (maxValueSize, updateValueCallback) => {
-        console.log("notifyNetworkConnectionCharacteristic - onSubscribe");
-        bleCallbacks.notifyCallback(true, maxValueSize, updateValueCallback);
-    },
-    onUnsubscribe: () => {
-        console.log("notifyNetworkConnectionCharacteristic - onUnsubscribe");
-        bleCallbacks.notifyCallback(false, 0, null);
-    },
-});
-
-const configurationService = new bleno.PrimaryService({
-    uuid: SERVICE_UUID,
-    characteristics: [writeCharacteristic, notifyCharacteristic],
-});
-
+const SOCKET_EVENT_BLE_ENABLE = "ble-enable"
+const SOCKET_EVENT_BLE_DISABLE = "ble-disable"
+const SOCKET_EVENT_NOTIFY = "notify"
 
 const io = new Server();
 
 let blenoReady = false;
 
-let nofifyStatus;
-let notifyMaxValueSize;
+let notifyMaxValueSize = 0;
 let notifyCallback;
+
+const configurationService = new bleno.PrimaryService({
+    uuid: SERVICE_UUID,
+    characteristics: [
+
+        // receives commands
+        new bleno.Characteristic({
+            uuid: WRITE_CHARACTERISTIC_UUID,
+            properties: ["write"],
+
+            onWriteRequest: (data, _offset, _withoutResponse, callback) => {
+                console.log("onWriteRequest write request: " + data.toString("utf-8"));
+                io.sockets.emit(EVENT_WRITE, data);
+                callback(bleno.Characteristic.RESULT_SUCCESS);
+            }
+        }),
+
+        // sends data
+        new bleno.Characteristic({
+            uuid: NOTIFY_CHARACTERISTIC_UUID,
+            properties: ["notify"],
+
+            onSubscribe: (maxValueSize, updateValueCallback) => {
+                console.log("notifyNetworkConnectionCharacteristic - onSubscribe");
+                notifyMaxValueSize = maxValueSize;
+                notifyCallback = updateValueCallback;
+            },
+            onUnsubscribe: () => {
+                console.log("notifyNetworkConnectionCharacteristic - onUnsubscribe");
+                notifyMaxValueSize = 0;
+                notifyCallback = undefined;
+            }
+        })]
+});
 
 bleno.on("stateChange", (state) => {
     console.log("on -> stateChange: " + state);
+
     if (state === "poweredOn") {
         blenoReady = true;
     }
@@ -65,19 +75,21 @@ bleno.on("advertisingStart", (err) => {
 });
 
 bleno.on("accept", () => {
-    io.sockets.emit("device-accepted");
+    io.sockets.emit(EVENT_DEVICE_ACCEPTED);
 });
 
 bleno.on("disconnect", () => {
-    io.sockets.emit("device-disconnected");
+    notifyMaxValueSize = 0;
+    notifyCallback = undefined;
+
+    io.sockets.emit(EVENT_DEVICE_DISCONNECTED);
 });
 
 io.on("connection", (socket) => {
-    socket.on("ble-enable", (data) => {
+    socket.on(SOCKET_EVENT_BLE_ENABLE, (data) => {
         if (blenoReady) {
             const deviceName = "rpi";
             const bluetooth_id = data.bluetooth_id;
-            const firstTimeBuffer = Buffer.from([data.firstTime ? 1 : 0]);
 
             const advertisementData = Buffer.concat([
                 Buffer.from([0x02, 0x01, 0x06]),
@@ -85,7 +97,7 @@ io.on("connection", (socket) => {
                 Buffer.from(deviceName),
                 Buffer.from([bluetooth_id.length + 1 + 1, 0xff]),
                 Buffer.from(bluetooth_id),
-                firstTimeBuffer,
+                Buffer.from([data.firstTime ? 1 : 0])
             ]);
 
             const scanResponseData = Buffer.concat([
@@ -95,7 +107,7 @@ io.on("connection", (socket) => {
                         .reverse()
                         .join(""),
                     "hex"
-                ), // Service UUID in little-endian format
+                ) // Service UUID in little-endian format
             ]);
 
             bleno.startAdvertisingWithEIRData(
@@ -106,77 +118,58 @@ io.on("connection", (socket) => {
                         console.error("Failed to start advertising:", err);
                     } else {
                         console.log("Advertising started successfully");
-                        io.emit("ble-enabled");
+                        io.emit(EVENT_BLE_ENABLED);
                     }
                 }
             );
         }
     });
 
-    socket.on("ble-disable", () => {
+    socket.on(SOCKET_EVENT_BLE_DISABLE, () => {
         bleno.stopAdvertising();
-        io.emit("ble-disabled");
+        io.emit(EVENT_BLE_DISABLED);
     });
 
-    socket.on("notify", (data) => {
-        if (nofifyStatus && notifyCallback != null) {
-            sendDataInChunks(data, notifyCallback, notifyMaxValueSize);
-        }
+    socket.on(SOCKET_EVENT_NOTIFY, (data) => {
+        sendDataInChunks(data);
     });
 });
 
-function sendDataInChunks(data, callback, chunkSize) {
-    const payloadSize = chunkSize - 3
-    const jsonData = JSON.stringify(data.data);
-    const key = data.key;
-    const totalPackets = Math.ceil(jsonData.length / payloadSize); // Calculate total packets
+function sendDataInChunks(data) {
+    if (notifyCallback && notifyMaxValueSize > 3) {
+        const payloadSize = notifyMaxValueSize - 3;
+        const jsonData = JSON.stringify(data.data);
+        const key = data.key;
+        const total = jsonData.length
+        const totalPackets = Math.ceil(total / payloadSize); // Calculate total packets
 
-    let offset = 0;
-    let packetID = 0;
+        let offset = 0;
+        let packetID = 0;
 
-    const sendNextChunk = () => {
-        if (offset >= jsonData.length) return;
+        while (offset < total) {
 
-        // Create the chunk payload
-        const payload = Buffer.from(jsonData.slice(offset, offset + payloadSize));
+            // Create the chunk payload
+            const payload = Buffer.from(jsonData.slice(offset, offset + payloadSize));
 
-        // Create a buffer for the chunk with the first three bytes reserved
-        const chunk = Buffer.alloc(payload.length + 3);
+            // Create a buffer for the chunk with the first three bytes reserved
+            const chunk = Buffer.alloc(payload.length + 3);
 
-        // Add the metadata
-        chunk[0] = key; // First byte: data.key
-        chunk[1] = packetID; // Second byte: packet ID
-        chunk[2] = totalPackets; // Third byte: total packets
+            // Add the metadata
+            chunk[0] = key; // First byte: data.key
+            chunk[1] = packetID; // Second byte: packet ID
+            chunk[2] = totalPackets; // Third byte: total packets
 
-        // Add the actual payload
-        payload.copy(chunk, 3);
+            // Add the actual payload
+            payload.copy(chunk, 3);
 
-        // Call the callback with the chunk
-        if (callback) {
-            callback(chunk);
+            notifyCallback(chunk);
+
+            // Update the offset and packet ID
+            offset += payloadSize;
+            packetID++;
         }
-
-        // Update the offset and packet ID
-        offset += payloadSize;
-        packetID++;
-
-        // Send the next chunk after a delay
-        setTimeout(sendNextChunk, 20);
-    };
-
-    sendNextChunk();
+    }
 }
-
-bleCallbacks.writeCallback = (data) => {
-    io.sockets.emit("write", data);
-};
-
-bleCallbacks.notifyCallback = (status, maxValueSize, callback) => {
-    nofifyStatus = status;
-    notifyMaxValueSize = maxValueSize;
-    notifyCallback = callback;
-    io.sockets.emit("notify");
-};
 
 const httpServer = http.createServer();
 io.attach(httpServer);
