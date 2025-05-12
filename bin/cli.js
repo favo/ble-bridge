059cb74bfc1ed7b655299b9850d4f9ebbe396e9d
Jon Håvard Gundersen <jonh@favo.no>
2025-05-12 15:45:41 +0200

    Hadde glemt å legge scriptet inn i pakken bare

--- bin/cli.js
+++ bin/cli.js
@@ -1,19 +1,3 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const { exec } = require('child_process');
-
-const scriptPath = path.join(__dirname, 'prepare-upgrade.sh');
-
-exec(`bash "${scriptPath}"`, (error, stdout, stderr) => {
-    if (error) {
-        console.error(`Upgrade script failed: ${error.message}`);
-        process.exit(1);
-    }
-    if (stderr) {
-        console.error(`stderr: ${stderr}`);
-    }
-    console.log(`stdout: ${stdout}`);
-});
-
 require("../app.js")
